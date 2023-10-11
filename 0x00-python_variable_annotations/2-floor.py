8ded053bb26fe5354eacd6fd43270c0ee0864c47
Angel291985 <angelsiluma.03@gmail.com>
2023-10-11 03:37:24 +0200

    python variable

--- 0x00-python_variable_annotations/2-floor.py
+++ 0x00-python_variable_annotations/2-floor.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-
+'''Task 2'''
 def floor(a: float) -> int:
     """computes the floor of a floating-point number."""
     return int(a)
--- 0x00-python_variable_annotations/3-to_str.py
+++ 0x00-python_variable_annotations/3-to_str.py
@@ -1,5 +1,5 @@
 #!/usr/bin/env python3
-
+'''Task 3'''
 def to_str(n: float) -> str:
     """Converts a floating-point number to a string.
     """
